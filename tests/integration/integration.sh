ab9918ef51987333f417fc2b9fe93849351853e6
David <drzheng@g.ucla.edu>
2020-04-18 10:39:26 -0700

    Added some basic integration tests 	-Basic tests to see if curl get and post work 	-Integrated bash into cmake
    Change-Id: I3d152d4915d9351fa1e7949d7c3ff4b9d425a652

---
+++ tests/integration/integration.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+ERROR=0
+
+#test server using integration.conf
+
+printf "server {\n\tlisten   8000;\n}" >integration.conf
+
+./server integration.conf &
+
+#sleep after starting command
+sleep 1
+
+#basic curl test
+if  curl -s localhost:8000 | \
+	tr "\n\r" " "| \
+	grep "GET / HTTP/1.1" | \
+	grep "Host:" | \
+	grep "User-Agent:" | \
+	grep "Accept:" > /dev/null;
+then
+	echo "	Basic curl test success."
+else
+	ERROR=1
+fi
+
+#curl post test
+if  curl -s -d val=0 localhost:8000 | \
+	tr "\n\r" " "| \
+	grep "POST / HTTP/1.1" | \
+	grep "Host:" | \
+	grep "User-Agent:" | \
+	grep "Accept:" | \
+	grep "Content-Length:" | \
+	grep "Content-Type:" | \
+	grep "val=0" > /dev/null; 
+then
+	echo "	Curl post test success."
+else
+	ERROR=1
+fi
+
+
+#cleanup
+pkill server
+rm integration.conf
+
+if [ $ERROR -eq 0 ]
+then 
+	echo "All tests finished and passed."
+	exit 1
+else 
+	echo "All tests finished with some failed."
+	exit 0
+fi
