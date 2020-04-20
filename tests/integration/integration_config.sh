bcb77d5eca83d91cbb1bb83968efeaf95038a317
Nelson <nsolano@g.ucla.edu>
2020-04-20 11:25:47 +0000

    fixed errors with previous tests
    Change-Id: I2b776f853578e630237d83fb79c6f34e5fd7aa0e

--- tests/integration/integration_config.sh
+++ tests/integration/integration_config.sh
@@ -4,44 +4,27 @@ ERROR=0
 
 #test server using integration.conf
 
-printf "server {\n\tlisten   8000;\n" >integration.conf
+printf "server {\n\tlisten 8010;\n\tserver_name foo.com;\n}" >integration.conf
+
 SERVER_EXECUTABLE=$1
 $SERVER_EXECUTABLE integration.conf &
 
-pid=$!
-while ps | grep "${pid}"
-do
-    sleep 1
-done
-wait $pid
-status=$?
-if [ $status -eq 0 ]
-then
-	echo "	Invalid config test success."
-else
-    ERROR=1
-fi
-#cleanup
-pkill server
-rm integration.conf
+#sleep after starting command
+sleep 1
 
-printf "server {\n\tlisten   800;\n}" >integration.conf
-SERVER_EXECUTABLE=$1
-$SERVER_EXECUTABLE integration.conf &
-
-pid=$!
-while ps | grep "${pid}"
-do
-    sleep 1
-done
-wait $pid
-status=$?
-if [ $status -eq 0 ]
+#basic curl test
+if  curl -s localhost:8010 | \
+	tr "\n\r" " "| \
+	grep "GET / HTTP/1.1" | \
+	grep "Host:" | \
+	grep "User-Agent:" | \
+	grep "Accept:" > /dev/null;
 then
-	echo "	Invalid config test success."
+	echo "	Basic curl test success."
 else
-    ERROR=1
+	ERROR=1
 fi
+
 #cleanup
 pkill server
 rm integration.conf
