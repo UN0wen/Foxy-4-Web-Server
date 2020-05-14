be2ea43a542b2f0ae843feb2227dd6d9b4b25d97
Haochen Li <joshuali1997@g.ucla.edu>
2020-05-14 10:38:30 +0000

    add StaticRequestHandler unit test and echo request handler unit test
    Change-Id: Ia624960c7b70bc9d778d086c328a9a3196fb6892

--- tests/echo_request_handler/echo_request_handler_test.cc
+++ tests/echo_request_handler/echo_request_handler_test.cc
@@ -6,63 +6,35 @@ class EchoRequestHandlerTest : public ::testing::Test
 protected:
     EchoRequestHandler request_handler;
     const int CONTENT_LENGTH = 19;
+    const int POST_REQUEST_LENGTH = 93;
     const char *post_request = "POST / HTTP/1.1\r\n\
 Content-Length: 19\r\n\
 Content-Type: application/json\r\n\
 Host: localhost:8000\r\n\r\n{'test_value': '1'}";
 
-    const int POST_REQUEST_LENGTH = 93;
-
-    Header header_1 = {
-        "Host",          //
-        "localhost:8000" //value
-    };
-
-    Header header_2 = {
-        "Content-Length", //name
-        "19"              //value
-    };
-
-    Header header_3 = {
-        "Content-Type",    //name
-        "application/json" //value
-    };
-
-    std::vector<Header> headers = {header_1, header_2, header_3};
-
-    Request request = {
-      Request::POST,                // std::string method;
-        "/",                   // std::string uri;
-      "1.1",
-        headers,               // std::vector<Header> headers;
-        "{'test_value': '1'}", // std::string data;
-        post_request           // const char raw_request
-    };
+    Request request;
+    
+    EchoRequestHandlerTest(){
+        NginxConfig config;
+        request_handler.Init("/echo/", config);
+        request.method_ = Request::MethodEnum::POST;
+        request.uri_ = "/";
+        request.http_version_ = "1.1";
+        request.headers_["Host"] = "localhost:8000";
+        request.headers_["Content-Length"] = "19";
+        request.headers_[ "Content-Type"] =  "application/json";
+        request.body_ = "{'test_value': '1'}";
+    }
 };
 
 TEST_F(EchoRequestHandlerTest, SendingGoodStatus)
 {
-    Response response;
-    request_handler.handle_request(request, response, RequestParser::result_type::good);
-    bool success_header_name = response.headers[1].name == "content-type";
-    bool success_content = response.content == post_request;
-    bool success_header_value = response.headers[1].value == "text/plain";
-    bool success_status = response.status == Response::ok;
-
-    EXPECT_TRUE(success_header_name && success_content && success_header_value && success_status);
+    Response response = request_handler.handle_request(request);
+    bool success_header_value = response.headers_["Content-Type"] == "text/plain";
+    bool success_status = response.code_ == Response::status_code::ok;
+    bool success_body = response.body_ == std::string(post_request);
+    EXPECT_TRUE(success_header_value  && success_status && success_body);
 }
 
-TEST_F(EchoRequestHandlerTest, SendingBadStatus)
-{
-    Response response;
-    response.status = Response::bad_request;
-    request_handler.handle_request(request, response, RequestParser::result_type::bad);
-    bool bad_header_name = response.headers[1].name == "content-type";
-    bool bad_content = response.content == post_request;
-    bool bad_header_value = response.headers[1].value == "text/plain";
-    bool bad_status = response.status == Response::bad_request;
-
-    EXPECT_TRUE(bad_header_name && bad_content && bad_header_value && bad_status);
-}
 
 
--- tests/static_request_handler/static_request_handler_test.cc
+++ tests/static_request_handler/static_request_handler_test.cc
@@ -5,19 +5,18 @@
 class StaticRequestHandlerTest : public ::testing::Test
 {
 protected:
-    StaticRequestHandler request_handler = StaticRequestHandler("./test_data", "/test");
+    StaticRequestHandler request_handler = StaticRequestHandler("./test_data/", "/test/");
     const char *get_request = "GET /test/ HTTP/1.1\r\n\
 Host: localhost:8000\r\n\r\n";
-  Response response;
-  std::map<std::string, std::string> headers_ = {{"Host", "localhost:8000"}};
-  Request request = {
-    Request::GET,                // std::string method;
-    "/test/",                   // std::string uri;
-    "1.1",
-    headers_,               // std::vector<Header> headers_;
-    "",                    // std::string data;
-    get_request           // const char raw_request
-  };
+
+    Request request;
+    StaticRequestHandlerTest(){
+        request.method_ = Request::MethodEnum::POST;
+        request.uri_ = "/test/";
+        request.http_version_ = "1.1";
+        request.headers_["Host"] = "localhost:8000";
+        request.body_ = "";
+    }
 };
 
 TEST_F(StaticRequestHandlerTest, GetIndexFile)
@@ -29,14 +28,13 @@ TEST_F(StaticRequestHandlerTest, GetIndexFile)
     char buf[512];
     while (is.read(buf, sizeof(buf)).gcount() > 0)
         content.append(buf, is.gcount());
+    request.uri_ = "/test/";
+    Response response = request_handler.handle_request(request);
+    bool success_header = response.headers_["Content-Type"] == "text/html";    
+    bool success_content = response.body_ == content;
+    bool success_status = response.code_ == Response::status_code::ok;
 
-    request_handler.handle_request(request, response, RequestParser::result_type::good);
-    bool success_header_name = response.headers_[1].name == "Content-Type";    
-    bool success_content = response.content == content;
-    bool success_header_value = response.headers_[1].value == "text/html";
-    bool success_status = response.status == Response::ok;
-
-    EXPECT_TRUE(success_header_name && success_content && success_header_value && success_status);
+    EXPECT_TRUE(success_header && success_status && success_content);
 }
 
 TEST_F(StaticRequestHandlerTest, GetIndexFileWithSpace)
@@ -49,102 +47,76 @@ TEST_F(StaticRequestHandlerTest, GetIndexFileWithSpace)
     while (is.read(buf, sizeof(buf)).gcount() > 0)
         content.append(buf, is.gcount());
 
-    const char *space_request = "POST /test/index+2.html HTTP/1.1\r\n\
-Host: localhost:8000\r\n\r\n";
-    request.raw_request = space_request;
+//     const char *space_request = "POST /test/index+2.html HTTP/1.1\r\n\
+// Host: localhost:8000\r\n\r\n";
+//     request.raw_request = space_request;
+    request.method_ = Request::MethodEnum::POST;
     request.uri_ = "/test/index+2.html";
 
-    request_handler.handle_request(request, response, RequestParser::result_type::good);
-    bool success_header_name = response.headers_[1].name == "Content-Type";
-    bool success_content = response.content == content;
-    bool success_header_value = response.headers_[1].value == "text/html";
-    bool success_status = response.status == Response::ok;
+    Response response = request_handler.handle_request(request);
+    bool success_header = response.headers_["Content-Type"] ==  "text/html";
+    bool success_content = response.body_ == content;
+    bool success_status = response.code_ == Response::status_code::ok;
 
-    EXPECT_TRUE(success_header_name && success_content && success_header_value && success_status);
+    EXPECT_TRUE(success_header && success_content  && success_status);
 }
 
 TEST_F(StaticRequestHandlerTest, FileNotFound)
 {
-    const char *not_found_request = "POST /test/nofile.html HTTP/1.1\r\n\
-Host: localhost:8000\r\n\r\n";
-    request.raw_request = not_found_request;
     request.uri_ = "/test/nofile.html";
+    request.method_ = Request::MethodEnum::POST;
+    Response response = request_handler.handle_request(request);
+    bool bad_status = response.code_ == Response::status_code::not_found;
 
-    request_handler.handle_request(request, response, RequestParser::result_type::good);
-    bool bad_header_name = response.headers_[1].name == "Content-Type";
-    bool bad_header_value = response.headers_[1].value == "text/html";
-    bool bad_status = response.status == Response::not_found;
-
-    EXPECT_TRUE(bad_header_name && bad_header_value && bad_status);
+    EXPECT_TRUE(bad_status);
 }
 
 TEST_F(StaticRequestHandlerTest, UrlEncodingError)
 {    
-    const char *unexpected_request = "POST /test/index%GG.html HTTP/1.1\r\n\
-Host: localhost:8000\r\n\r\n";
-    request.raw_request = unexpected_request;
-    request.uri_ = "/test/index%GG.html";
     
-    request_handler.handle_request(request, response, RequestParser::result_type::good);
-    bool header_value = response.headers_[1].value == "text/html";
-    bool status = response.status == Response::bad_request;
 
-    EXPECT_TRUE(header_value && status);
+    request.uri_ = "/test/index%GG.html";
+    request.method_ = Request::MethodEnum::POST;
+
+    Response response = request_handler.handle_request(request);
+    bool status = response.code_ == Response::status_code::bad_request;
+
+    EXPECT_TRUE(status);
 }
 
 TEST_F(StaticRequestHandlerTest, UnexpectedEndOfURL)
 {    
-    const char *unexpected_request = "POST /test/index.html%20%2 HTTP/1.1\r\n\
-Host: localhost:8000\r\n\r\n";
-    request.raw_request = unexpected_request;
+    
+    request.method_ = Request::MethodEnum::POST;
     request.uri_ = "/test/index.html%20%2";
 
-    request_handler.handle_request(request, response, RequestParser::result_type::good);
-    bool header_value = response.headers_[1].value == "text/html";
-    bool status = response.status == Response::bad_request;
+    Response response = request_handler.handle_request(request);
+    bool status = response.code_ == Response::status_code::bad_request;
 
-    EXPECT_TRUE(header_value && status);
+    EXPECT_TRUE(status);
 }
 
 TEST_F(StaticRequestHandlerTest, UrlRelativePath)
 {    
-    const char *relative_path_request = "POST ./test/index.html HTTP/1.1\r\n\
-Host: localhost:8000\r\n\r\n";
-    request.raw_request = relative_path_request;
+    
+    request.method_ = Request::MethodEnum::POST;
     request.uri_ = "./test/index.html";
 
-    request_handler.handle_request(request, response, RequestParser::result_type::good);
-    bool header_value = response.headers_[1].value == "text/html";
-    bool status = response.status == Response::bad_request;
+    Response response = request_handler.handle_request(request);
+    bool status = response.code_ == Response::status_code::bad_request;
 
-    EXPECT_TRUE(header_value && status);
+    EXPECT_TRUE(status);
 }
 
 TEST_F(StaticRequestHandlerTest, WrongPath)
 {    
-    const char *wrong_path_request = "POST /not_test/index.html HTTP/1.1\r\n\
-Host: localhost:8000\r\n\r\n";
-    request.raw_request = wrong_path_request;
+    
+    request.method_ = Request::MethodEnum::POST;
     request.uri_ = "/not_test/index.html";
 
-    request_handler.handle_request(request, response, RequestParser::result_type::good);
-    bool header_value = response.headers_[1].value == "text/html";
-    bool status = response.status == Response::internal_server_error;
+    Response response = request_handler.handle_request(request);
+    bool status = response.code_ == Response::internal_server_error;
 
-    EXPECT_TRUE(header_value && status);
-}
-
-TEST_F(StaticRequestHandlerTest, BadRequest)
-{    
-    const char *bad_request = "POST /test/index.html BAD/1.1\r\n\
-Host: localhost:8000\r\n\r\n";
-    request.raw_request = bad_request;
-    request.uri_ = "/test/index.html";
-
-    request_handler.handle_request(request, response, RequestParser::result_type::bad);
-    bool header_value = response.headers_[1].value == "text/html";
-    bool status = response.status == Response::bad_request;
-
-    EXPECT_TRUE(header_value && status);
+    EXPECT_TRUE(status);
 }
 
