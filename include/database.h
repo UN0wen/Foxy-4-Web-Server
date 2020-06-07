cab88455980cc3503f3b6bf04a5ecaba05fcb2f4
David <drzheng@g.ucla.edu>
2020-06-07 09:05:40 -0700

    Prepared SQL queries to protect against SQL injection
      -INSERT and SELECT queries are now prepared with sqlite3_prepare
      -Create table left alone because it is hardcoded
      -Loop in db_select_pass might not be necessary
    	-the select query shouldn't be able to get more than 1 row
    	-in the off chance that 2+ rows are somehow selected, the
             function will return the result from the last row
      -Parameterized user inputs
    
    Change-Id: If65d2c2bf9a019ee4db455ff3c4f5c1fea19825f

--- include/database.h
+++ include/database.h
@@ -20,7 +20,7 @@ void db_create()
     char *zErrMsg = 0;
     const char *sql;
 
-    rc = sqlite3_open("auth.db", &db);
+    sqlite3_open("auth.db", &db);
 
     sql = "CREATE TABLE AUTH("  \
     "USER TEXT NOT NULL," \
@@ -30,53 +30,67 @@ void db_create()
     rc = sqlite3_exec(db, sql, 0, 0, &zErrMsg);
 
     if( rc != SQLITE_OK ){
-      fprintf(stderr, "SQL msg: %s\n", zErrMsg);
-      sqlite3_free(zErrMsg);
-    } 
+      	fprintf(stderr, "SQL msg: %s\n", zErrMsg);
+      	sqlite3_free(zErrMsg);
+    }
     sqlite3_close(db);
 }
 
-void db_insert(std::string user, std::string pass)
-{
+void db_insert(std::string user, std::string pass){
 	sqlite3 *db;
     int rc;
-    char *zErrMsg = 0;
     std::string sql;
+    sqlite3_stmt *query;
 
-    rc = sqlite3_open("auth.db", &db);
+    sqlite3_open("auth.db", &db);
 
     sql = "INSERT INTO AUTH(user,pass) "  \
-    "VALUES ('"+ user + "', '"+ pass +"');";   
+    "VALUES (?1, ?2);";   
 
-    rc = sqlite3_exec(db, sql.c_str(), 0, 0, &zErrMsg);
+	rc = sqlite3_prepare_v2(db, sql.c_str(), -1, &query, NULL);    
+    sqlite3_bind_text(query, 1, user.c_str(), user.size(), NULL);
+    sqlite3_bind_text(query, 2, pass.c_str(), pass.size(), NULL);
 
     if( rc != SQLITE_OK ){
-      fprintf(stderr, "SQL msg: %s\n", zErrMsg);
-      sqlite3_free(zErrMsg);
-    } 
-
+      	printf("SQL msg: %s\n", sqlite3_errmsg(db));
+      	return;
+    }
+    rc = sqlite3_step(query);
+    if (rc != SQLITE_DONE){
+	    printf("SQL msg: %s\n", sqlite3_errmsg(db));
+	}
+	sqlite3_finalize(query);
     sqlite3_close(db);
 }
 
-std::string db_select_pass(std::string user)
-{
+std::string db_select_pass(std::string user){
     sqlite3 *db;
     int rc;
     char *zErrMsg = 0;
     std::string sql;
+    sqlite3_stmt *query;
     std::string data;
 
-    rc = sqlite3_open("auth.db", &db);
+    sqlite3_open("auth.db", &db);
 
-    sql = "SELECT pass from AUTH WHERE user='"+ user +"'";
+    sql = "SELECT pass from AUTH WHERE user=?1";
+    rc = sqlite3_prepare_v2(db, sql.c_str(), -1, &query, NULL);
+    sqlite3_bind_text(query, 1, user.c_str(), user.size(), NULL);
 
-    rc = sqlite3_exec(db, sql.c_str(), callback_select, &data, &zErrMsg);
-    
-    if( rc != SQLITE_OK ) {
-      fprintf(stderr, "SQL error: %s\n", zErrMsg);
-      sqlite3_free(zErrMsg);
+    if( rc != SQLITE_OK ){
+      	printf("SQL SELECT msg: %s\n", sqlite3_errmsg(db));
+      	return "";
     }
-    
+
+    rc = sqlite3_step(query);
+    while(rc == SQLITE_ROW){
+    	data = (const char *)sqlite3_column_text(query, 0);
+    	rc = sqlite3_step(query);
+    }
+    if (rc != SQLITE_DONE){
+	    printf("SQL msg: %s\n", sqlite3_errmsg(db));
+	}
+	sqlite3_finalize(query);
     sqlite3_close(db);
 
     return data;
